d15b372538988900355de363b06d68d441faa65b
league <jacktu2718@gmail.com>
2023-09-21 16:56:03 -0700

    still working on chuckle clicker

---
+++ src/_01_chuckle_clicker/ChuckleClicker.java
@@ -0,0 +1,18 @@
+package _01_chuckle_clicker;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class ChuckleClicker {
+	public static void main(String[] args) {
+		
+	}
+	static void makeButtons() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.add(panel);
+		JButton button1 = new JButton();
+		JButton button2 = new JButton();
+	}
+}
